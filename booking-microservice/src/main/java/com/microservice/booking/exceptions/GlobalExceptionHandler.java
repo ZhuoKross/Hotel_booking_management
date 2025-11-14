61acbb150b11d71dbdb45c820514ead7fe495995
Michael <mgarnica.oficial@gmail.com>
2025-11-13 19:25:29 -0500

    refactor(BookingMicroservice): changing the feign clients and some parts of the RoomsMicroservice and HostMicroservice
    implementing the correct response objects for the feign clients. Also, changing some parts of the controllers of the microservices Room and Hosts

---
+++ booking-microservice/src/main/java/com/microservice/booking/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,44 @@
+package com.microservice.booking.exceptions;
+
+
+import com.microservice.booking.Utils.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Response<String>> handleIllegalArgument () {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Response<String>("Bad argument passed. Verify and try again.", LocalDateTime.now(), "no data"));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Response<Map<String, String>>> handleValidations (MethodArgumentNotValidException errors){
+        Map<String, String> validationsErrors = new HashMap<>();
+
+        errors.getBindingResult().getFieldErrors().forEach((err) ->{
+            validationsErrors.put(err.getField(), err.getDefaultMessage());
+        });
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Response<Map<String, String>>("Malformed request body", LocalDateTime.now(), validationsErrors));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Response<String>> handleMalformedRequestBody (){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Response<String>("Malformed request body. It couldn't be possible deserialize the object", LocalDateTime.now(), "no data"));
+    }
+
+    @ExceptionHandler(BookingNotFoundException.class)
+    public ResponseEntity<Response<String>> handleBookingNotFoundException(){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new Response<String>("Booking not Found. Try again later", LocalDateTime.now(), "no data"));
+    }
+}
